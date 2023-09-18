bfffe8da7effc97eaa32a6e84f05781bb442b9ed
ydah <13041216+ydah@users.noreply.github.com>
2023-09-18 15:39:43 +0900

    Bump version to v2.24.0

--- lib/rubocop/factory_bot/version.rb
+++ lib/rubocop/factory_bot/version.rb
@@ -4,7 +4,7 @@ module RuboCop
   module FactoryBot
     # Version information for the factory_bot RuboCop plugin.
     module Version
-      STRING = '2.23.1'
+      STRING = '2.24.0'
     end
   end
 end
